87c8fe49cbaefa4109b7788699cb4b6c0fbf60f2
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-07-17 03:41:09 +1000

    styled the login and register pages

--- client/src/Pages/Auth/Login.js
+++ client/src/Pages/Auth/Login.js
@@ -1,35 +1,41 @@
 import React, {useState, useContext} from 'react';
 import AuthContext from "../../Contexts/AuthContext";
+import axios from "axios";
+import {useHistory} from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import axios from "axios";
-import {useHistory} from "react-router-dom";
+import TitleImg from "../../Images/login.png";
+import "./Auth.css";
 
-
-const useStyles = makeStyles(theme => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 export default function SignIn() {
@@ -72,11 +78,8 @@ export default function SignIn() {
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
                 <Typography component="h1" variant="h5">
-                    Sign in
+                <img src={TitleImg} className="title-logo" alt="Gamerspace Logo"/>
                 </Typography>
                 <form className={classes.form} onSubmit={handleOnSubmit}>
                     <TextField
@@ -104,6 +107,7 @@ export default function SignIn() {
                         fullWidth
                         variant="contained"
                         color="primary"
+                        size="large"
                         className={classes.submit}
                     >
                         Sign In
--- client/src/Pages/Auth/Register.js
+++ client/src/Pages/Auth/Register.js
@@ -1,33 +1,41 @@
 import React, {useState} from 'react';
+import validator from "validator";
+import axios from "axios";
+import AuthContext from "../../Contexts/AuthContext";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import validator from "validator";
-import axios from "axios";
+import TitleImg from "../../Images/register3.png";
+import "./Auth.css";
 
-const useStyles = makeStyles(theme => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 export default function Register() {
@@ -79,11 +87,8 @@ export default function Register() {
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
                 <Typography component="h1" variant="h5">
-                    Register
+                <img src={TitleImg} className="title-logo" alt="Gamerspace Logo"/>
                 </Typography>
                 <form className={classes.form} onSubmit={handleOnSubmit}>
                     <TextField
@@ -126,6 +131,7 @@ export default function Register() {
                         fullWidth
                         variant="contained"
                         color="primary"
+                        size="large"
                         className={classes.submit}
                     >
                         Sign In
