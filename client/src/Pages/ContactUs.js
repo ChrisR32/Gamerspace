ea10b701e69ab83c7686e7608478a0d255044075
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-07-29 14:12:28 +1000

    styled the contact us page

--- client/src/Pages/ContactUs.js
+++ client/src/Pages/ContactUs.js
@@ -6,15 +6,18 @@ export default function Home() {
     return (
         <div className="top-div login-bottom">
                     <div className="main-content">
-            <h1>Contact us</h1>
+            <h1>Get in touch</h1>
+                        <h4>Have a question? Need assistance? We are here to help!</h4>
+
             <div className="row">
-            
             <div className="view">
+                
             <ReactContactForm to="morriscnm@hotmail.com"
             titlePlaceholder="Title"
             contentsRows="6"
             contentsPlaceholder="Contents"
-            buttonText="Send" />
+            buttonText="SEND EMAIL" />
+            <br/>
 </div>
 </div>
 </div>
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -6,15 +6,11 @@ export default function Home() {
     return (
         <div className="top-div login-bottom">
                     <div className="main-content">
-            <h1>Contact us</h1>
+            <h1>Home</h1>
             <div className="row">
             
             <div className="view">
-            <ReactContactForm to="morriscnm@hotmail.com"
-            titlePlaceholder="Title"
-            contentsRows="6"
-            contentsPlaceholder="Contents"
-            buttonText="Send" />
+  
 </div>
 </div>
 </div>
