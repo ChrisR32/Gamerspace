c6f5fd00332435819bd61773ab79394a8e0ce3ad
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-07-30 21:42:21 +1000

    added pres

---
+++ client/build/precache-manifest.a49639b26490f0c76a5f71954b99c811.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7e0c53774894943051c2173cdbb151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da759eeab28faa63acb7",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "309df609e23aa91c95c4",
+    "url": "/static/css/main.7c8673bd.chunk.css"
+  },
+  {
+    "revision": "da759eeab28faa63acb7",
+    "url": "/static/js/2.24b9a5ea.chunk.js"
+  },
+  {
+    "revision": "5403b55c1cb422a72be68a013c0b9049",
+    "url": "/static/js/2.24b9a5ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309df609e23aa91c95c4",
+    "url": "/static/js/main.26bcc53c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c5efb82dcccc7f06ae379bdc9fdf00e3",
+    "url": "/static/media/ducko.c5efb82d.gif"
+  },
+  {
+    "revision": "a0229af31fe08231f8b5ed31186c9c12",
+    "url": "/static/media/history1.a0229af3.png"
+  },
+  {
+    "revision": "d61aadbc1a1e11577f03c706c5a6035b",
+    "url": "/static/media/history2.d61aadbc.png"
+  },
+  {
+    "revision": "638491da9d29acddc747928061c66c8c",
+    "url": "/static/media/history3.638491da.png"
+  },
+  {
+    "revision": "eb23ffe9b8499e75b0a65e7bc55d0487",
+    "url": "/static/media/login4.eb23ffe9.png"
+  },
+  {
+    "revision": "48dddadfad5ab9428b9db000533ebd1a",
+    "url": "/static/media/logo5.48dddadf.png"
+  },
+  {
+    "revision": "f47039e60a784d58d1ae29afa21694fe",
+    "url": "/static/media/slide1.f47039e6.png"
+  },
+  {
+    "revision": "facde67a2b7e964b6867b56a4aa73dbe",
+    "url": "/static/media/slide2.facde67a.png"
+  },
+  {
+    "revision": "b25b66ea9dc17656d5d48e5f141fd79f",
+    "url": "/static/media/slide3.b25b66ea.png"
+  },
+  {
+    "revision": "ef4a909bbb207f832ec207c35aafeecb",
+    "url": "/static/media/slide4.ef4a909b.png"
+  },
+  {
+    "revision": "f3a4b9f03698160326144a8134cbe1c6",
+    "url": "/static/media/slide5.f3a4b9f0.png"
+  },
+  {
+    "revision": "0d946482527fc92cdb404ee94e08bf04",
+    "url": "/static/media/slide6.0d946482.png"
+  }
+]);
